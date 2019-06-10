310e4ebdbfb53d0478b0e4518373c6ffcc2a78b9
cronk2gumpy <47121670+cronk2gumpy@users.noreply.github.com>
2019-06-10 15:56:07 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,6 @@
 from test import *
 from searcher import *
 
-crash_url = searcher('Crash Bandicoot')
-data = data_extractor(crash_url)
-print_game(data)
-
 with open('output.csv', 'w') as f:
     f.write('Title, Main Story, Main + Extras, Completionist, Developer, Publisher, Platform(s), Genre(s)\n')
 
